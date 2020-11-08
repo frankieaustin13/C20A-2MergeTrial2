47f091fb08f0a3fbaecee3b08e50b5cf6b3ffd63
Frankie Austin <frankieaustin@mymail.mines.edu>
2020-11-07 20:08:31 -0700

    Added the DifferentPhrases branch

--- src/TheMotivator.java
+++ src/TheMotivator.java
@@ -1,13 +1,13 @@
 public class TheMotivator {
 	public void feedback(int score) {
-		if (score == 50)
-			System.out.println("You're awesome");
-		else if (score > 45)
-			System.out.println("That's great");
-		else if (score > 30)
-			System.out.println("That's good ");
+		if (score == 100)
+			System.out.println("Oh watch out. Badass");
+		else if (score > 90)
+			System.out.println("Ayyee my guy!");
+		else if (score > 60)
+			System.out.println("Well, at least you passed.");
 		else
-			System.out.println("Well, what can I say?");
+			System.out.println("That's CLIPPED.");
 	}
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
